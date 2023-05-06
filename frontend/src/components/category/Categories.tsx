97078b74d716341385d72b50bc2d9183f3063cc4
Femi Adesola Oyinloye <75606768+FemiAdesola@users.noreply.github.com>
2023-05-06 18:06:30 +0300

    perform function to show data of categories in frontend

--- frontend/src/components/category/Categories.tsx
+++ frontend/src/components/category/Categories.tsx
@@ -1,22 +1,55 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import axios, { AxiosResponse } from "axios";
 import { CategoryDTO } from "../types/category";
 import { urlCategories } from "../common/endpoint";
+import ResultList from "../utils/ResultList";
+import Button from "../features/Button";
 
 const Categories = () => {
+  const [categories, setCategories] = useState<CategoryDTO[]>();
+
   useEffect(() => {
     axios.get(urlCategories)
       .then((response: AxiosResponse<CategoryDTO[]>) => {
-      console.log(response.data);
+        // console.log(response.data);
+        setCategories(response.data);
     });
   }, [])
   return (
-    <div className="container">
+    <div>
       <h3>Categories</h3>
       <Link className="btn btn-primary" to="create">
-        Create Category
+        Create Categories
       </Link>
+      <ResultList list={categories}>
+        <table className=" table table-striped">
+          <thead>
+            <tr>
+              <th>Options</th>
+              <th>Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            {categories?.map((category) => (
+              <tr key={category.id}>
+                <td>
+                  <Link
+                    className="btn btn-success"
+                    to={`/categories/${category.id}`}
+                  >
+                    Update
+                  </Link>
+                  <Button className="btn btn-danger">Delete</Button>
+                </td>
+                <td>
+                  <strong>{category.name}</strong>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </ResultList>
     </div>
   );
 };
