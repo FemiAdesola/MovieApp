ec6cbf005dd7ca33bee7a51bdc8f24f281b1a2e2
Femi Adesola Oyinloye <75606768+FemiAdesola@users.noreply.github.com>
2023-05-08 17:06:20 +0300

    updated createactor

--- frontend/src/components/actors/CreateActor.tsx
+++ frontend/src/components/actors/CreateActor.tsx
@@ -1,42 +1,44 @@
-import React, { useState } from 'react';
-import ActorForm from './ActorForm';
-import { CreateActorDTO } from '../types/actor';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
-import { convertActorToFormData } from '../features/convertToFormData';
-import { urlActors } from '../common/endpoint';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+import ActorForm from "./ActorForm";
+import { CreateActorDTO } from "../types/actor";
+import { convertActorToFormData } from "../features/convertToFormData";
+import { urlActors } from "../common/endpoint";
+import DisplayError from "../utils/DisplayError";
 
 const CreateActor = () => {
-    const [errors, setErrors] = useState<string[]>([]);
-    const navigate = useNavigate();
+  const [errors, setErrors] = useState<string[]>([]);
+  const navigate = useNavigate();
 
-   const create = async (actor: CreateActorDTO) => {
-     try {
-       const formData = convertActorToFormData(actor);
+  const create = async (actor: CreateActorDTO) => {
+    try {
+      const formData = convertActorToFormData(actor);
 
-       await axios({
-         method: "post",
-         url: urlActors,
-         data: formData,
-         headers: { "Content-Type": "multipart/form-data" },
-       });
-       navigate("/actors");
-     } catch (error) {
-       if (error && error.response) {
-         setErrors(error.response.data);
-       }
-     }
-   };
-    return (
-      <div className="container">
-        <h3>Create actors</h3>
-        {/* <DisplayErrors errors={errors} /> */}
-        <ActorForm
-          model={{ name: "", dateOfBirth: undefined }}
-          onSubmit={async (values) => await create(values)}
-        />
-      </div>
-    );
+      await axios({
+        method: "post",
+        url: urlActors,
+        data: formData,
+        headers: { "Content-Type": "multipart/form-data" },
+      });
+      navigate("/actors");
+    } catch (error) {
+      if (error && error.response) {
+        setErrors(error.response.data);
+      }
+    }
+  };
+  return (
+    <div className="container">
+      <h3>Create actors</h3>
+      <DisplayError errors={errors} />
+      <ActorForm
+        model={{ name: "", dateOfBirth: undefined }}
+        onSubmit={async (values) => await create(values)}
+      />
+    </div>
+  );
 };
 
 export default CreateActor;
