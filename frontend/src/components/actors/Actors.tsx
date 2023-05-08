07d54598fe04f7ec280665ba09f23a92fa1c9517
Femi Adesola Oyinloye <75606768+FemiAdesola@users.noreply.github.com>
2023-05-08 16:30:54 +0300

    creates  get method for actors in frontend

--- frontend/src/components/actors/Actors.tsx
+++ frontend/src/components/actors/Actors.tsx
@@ -1,14 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Entity from '../utils/Entity';
+import { ActorDTO } from '../types/actor';
+import { urlActors } from '../common/endpoint';
 
 const Actors = () => {
     return (
-      <div className="container">
-        <h3>Actors</h3>
-        <Link className="btn btn-primary" to="create">
-          Create Actor
-        </Link>
-      </div>
+      <>
+        <Entity<ActorDTO>
+          url={urlActors}
+          createURL="/actors/create"
+          title="Actors"
+          entityName="Actor"
+        >
+          {(actors, buttons) => (
+            <>
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>Name</th>
+                </tr>
+              </thead>
+              <tbody>
+                {actors?.map((actor) => (
+                  <tr key={actor.id}>
+                    <td>{buttons(`/actors/update/${actor.id}`, actor.id)}</td>
+                    <td>{actor.name}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </>
+          )}
+        </Entity>
+      </>
     );
 };
 
