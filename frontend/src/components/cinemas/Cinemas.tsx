f2ab743096784776184f44a611103fd6a8bd89d9
Femi Adesola Oyinloye <75606768+FemiAdesola@users.noreply.github.com>
2023-05-09 10:59:03 +0300

    updated cinemas ui

--- frontend/src/components/cinemas/Cinemas.tsx
+++ frontend/src/components/cinemas/Cinemas.tsx
@@ -1,10 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { CinemasDTO } from '../types/cinemas';
+import { urlMovieCinemas } from '../common/endpoint';
+import Entity from '../utils/Entity';
 
 const Cinemas = () => {
     return (
       <div className="container">
         <h3>Cinemas</h3>
+        <Entity<CinemasDTO>
+          url={urlMovieCinemas}
+          createURL="/cinemas/create"
+          title="Cinemas"
+          entityName="Movie Cinemas"
+        >
+          {(cinemas, buttons) => (
+            <>
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>Name</th>
+                </tr>
+              </thead>
+              <tbody>
+                {cinemas?.map((cinema) => (
+                  <tr key={cinema.id}>
+                    <td>
+                      {buttons(`/cinemas/update/${cinema.id}`, cinema.id)}
+                    </td>
+                    <td>{cinema.name}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </>
+          )}
+        </Entity>
+
         <Link className="btn btn-primary" to="create">
           Create cinemas
         </Link>
